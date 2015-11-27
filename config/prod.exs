15e5bb2573d558d2c046366cd6d7ffa9078c48f6
Franco Barbeite <francob411@gmail.com>
2015-11-27 17:36:20 -0500

    syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ config :unblogaso_api, UnblogasoApi.Endpoint,
   http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: "mighty-bayou-6283.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
